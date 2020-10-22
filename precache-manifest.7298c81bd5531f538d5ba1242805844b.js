2680f5f728584bd8704fab57fc952e7e069077aa
averdin2 <alexverdin01@gmail.com>
2020-10-21 21:26:14 -0700

    Updates

---
+++ precache-manifest.7298c81bd5531f538d5ba1242805844b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322fd04f85323d2a9c5f44fa89c432bf",
+    "url": "/React_Pomodoro/index.html"
+  },
+  {
+    "revision": "02835127ec7a38fbab36",
+    "url": "/React_Pomodoro/static/css/main.1fde1920.chunk.css"
+  },
+  {
+    "revision": "34292e67be22836a3da9",
+    "url": "/React_Pomodoro/static/js/2.746eb702.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Pomodoro/static/js/2.746eb702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02835127ec7a38fbab36",
+    "url": "/React_Pomodoro/static/js/main.adfc2cd8.chunk.js"
+  },
+  {
+    "revision": "f32e3a0516a2dd80e5c0",
+    "url": "/React_Pomodoro/static/js/runtime-main.2959a50a.js"
+  }
+]);
